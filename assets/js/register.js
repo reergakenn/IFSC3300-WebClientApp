b4230102a0d1d735eb6a6968df01c0df9ae4ba72
reergakenn <57770264+reergakenn@users.noreply.github.com>
2020-03-24 22:01:57 -0500

    Add files via upload

---
+++ assets/js/register.js
@@ -0,0 +1,76 @@
+// Register an Account-ch 6-slide 49 
+
+"use strict"
+var $ =
+    function (id) {return document.getElementById(id) ; };
+
+var processEntries = function () {
+    var isValid = true;
+
+    var email = $("email_address") .value;
+    var phone = $("phone") .value;
+    var country = $("country") .value;
+    var contact = "Text";
+    if ($("email") .checked) { contact = "Email"; }
+    if ($("none") .checked) { contact = "None"; }
+    var terms = $ ("terms") .checked;
+
+    if (email =="") {
+        $("email_address") .nextElementSibling.firstChild.
+            nodeValue = "This field is required.";
+        isValid = false; }
+    else {
+        $("email_address") .nextElementSibling.firstChild.
+            nodeValue = ""; }
+
+    if (phone =="") {
+        $("phone") .nextElementSibling.firstChild.nodeValue = 
+            "This field is required.";
+        isValid = false; }
+    else {
+        $("phone") .nextElementSibling.firstChild.nodeValue =
+         ""; }
+    if (country =="") {
+    $("country") .nextElementSibling.firstChild.nodeValue = 
+        "Please select a country.";
+    isValid = false; }
+    else {
+    $("country") .nextElementSibling.firstChild.nodeValue =
+        ""; }
+    if (terms == false) {
+        $("terms") .nextElementSibling.firstChild.nodeValue = 
+            "This box must be checked.";
+        isValid = false; }
+    else {
+        $("terms") .nextElementSibling.firstChild.nodeValue =
+            ""; }
+
+        //submit the form if all fields are valid- slide 51
+        if (isValid == true) {
+            $("registration_form") .submit(); }
+            
+     };
+     var resetForm = function() {
+        $("registration_form") .reset();
+        $("email_address") .nextElementSibling.firstChild.
+        nodeValue = "*";
+        $("phone") .nextElementSibling.firstChild.
+        nodeValue = "*";
+        $("country") .nextElementSibling.firstChild.
+        nodeValue = "*";
+        $("terms") .nextElementSibling.firstChild.
+        nodeValue = "*";
+        $("email_address") .focus();
+    };
+    window.onload =function() {
+        $("register").onclick = processEntries;
+        $("reset_form").onclick = resetForm;
+        $("email_address").focus();
+    };
+
+
+    
+
+
+
+}
