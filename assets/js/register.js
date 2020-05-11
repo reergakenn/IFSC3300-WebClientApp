a497063721f96be3a7d07e97a41e4a16fa95f0ab
reergakenn <57770264+reergakenn@users.noreply.github.com>
2020-05-11 17:40:00 -0500

    Update register.js

--- assets/js/register.js
+++ assets/js/register.js
@@ -1,76 +1,62 @@
-// Register an Account-ch 6-slide 49 
+"use strict";
+var $ = function(id) { return document.getElementById(id); };
 
-"use strict"
-var $ =
-    function (id) {return document.getElementById(id) ; };
-
-var processEntries = function () {
-    var isValid = true;
-
-    var email = $("email_address") .value;
-    var phone = $("phone") .value;
-    var country = $("country") .value;
+var processEntries = function() {   
+    var header = "";
+    var html = "";
+    var required = "<span>Required field</span>";
+    var msg = "Please review your entries and complete all required fields";
+    var email = $("email_address").value;
+    var phone = $("phone").value;
+    var country = $("country").value;
     var contact = "Text";
-    if ($("email") .checked) { contact = "Email"; }
-    if ($("none") .checked) { contact = "None"; }
-    var terms = $ ("terms") .checked;
+    if ($("email").checked) { contact = "Email"; }
+    if ($("mobile").checked) { contact = "Mobile"; }
+    if ($("none").checked) { contact = "None"; }
+    var terms = $("terms").checked;
+	var comments = $("comments").value;
 
-    if (email =="") {
-        $("email_address") .nextElementSibling.firstChild.
-            nodeValue = "This field is required.";
-        isValid = false; }
-    else {
-        $("email_address") .nextElementSibling.firstChild.
-            nodeValue = ""; }
-
-    if (phone =="") {
-        $("phone") .nextElementSibling.firstChild.nodeValue = 
-            "This field is required.";
-        isValid = false; }
-    else {
-        $("phone") .nextElementSibling.firstChild.nodeValue =
-         ""; }
-    if (country =="") {
-    $("country") .nextElementSibling.firstChild.nodeValue = 
-        "Please select a country.";
-    isValid = false; }
-    else {
-    $("country") .nextElementSibling.firstChild.nodeValue =
-        ""; }
+    if (email == "") {
+        email = required;          
+        header = msg;
+    }
+	if (phone == "") {
+        phone = required;         
+        header = msg;
+    }
+	if (country == "") {
+        country = required;         
+        header = msg;
+    }
     if (terms == false) {
-        $("terms") .nextElementSibling.firstChild.nodeValue = 
-            "This box must be checked.";
-        isValid = false; }
-    else {
-        $("terms") .nextElementSibling.firstChild.nodeValue =
-            ""; }
-
-        //submit the form if all fields are valid- slide 51
-        if (isValid == true) {
-            $("registration_form") .submit(); }
-            
-     };
-     var resetForm = function() {
-        $("registration_form") .reset();
-        $("email_address") .nextElementSibling.firstChild.
-        nodeValue = "*";
-        $("phone") .nextElementSibling.firstChild.
-        nodeValue = "*";
-        $("country") .nextElementSibling.firstChild.
-        nodeValue = "*";
-        $("terms") .nextElementSibling.firstChild.
-        nodeValue = "*";
-        $("email_address") .focus();
-    };
-    window.onload =function() {
-        $("register").onclick = processEntries;
-        $("reset_form").onclick = resetForm;
-        $("email_address").focus();
-    };
-
-
+        terms = required;
+        header = msg;
+    }
     
+    $("registration_header").firstChild.nodeValue = header;
+    if (header == msg) {     
+        html = html + "<tr><td>Email:</td><td>" + email + "</td></tr>";
+        html = html + "<tr><td>Phone:</td><td>" + phone + "</td></tr>";
+        html = html + "<tr><td>Country:</td><td>" + country + "</td></tr>";
+        html = html + "<tr><td>Contact:</td><td>" + contact + "</td></tr>";
+        html = html + "<tr><td>Terms:</td><td>" + terms + "</td></tr>";
+        html = html + "<tr><td>Comment:</td><td>Entry length = " + comments.length + "</td></tr>";
+        $("registration_info").innerHTML = html;
+    } else {
+        $("registration_info").innerHTML = "";
+        $("registration_form").submit();
+    }
+};
 
+var resetForm = function() {
+    $("registration_form").reset();
+    $("registration_header").firstChild.nodeValue = "";
+    $("registration_info").innerHTML = "";
+    $("email_address").focus();
+};
 
-
-}
+window.onload = function() {
+    $("register").onclick = processEntries;
+    $("reset_form").onclick = resetForm;    
+    $("email_address").focus();
+};
